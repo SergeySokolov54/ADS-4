dda25f5a421501b76ba24ca461da50cd37948380
SergeySokolov54 <116427633+SergeySokolov54@users.noreply.github.com>
2023-03-24 01:18:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,19 @@
 // Copyright 2021 NNTU-CS
+
+int sorted(int* arr, int size) {
+    int k = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                k = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = k;
+            }
+        }
+    }
+    return *arr;
+}
+
 int countPairs1(int *arr, int len, int value) {
   sorted(arr, len);
     int k = 0;
