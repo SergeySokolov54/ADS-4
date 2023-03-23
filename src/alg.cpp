743ad50872d25b0d83848b785545bd39c32a9de6
SergeySokolov54 <116427633+SergeySokolov54@users.noreply.github.com>
2023-03-24 00:50:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -27,7 +27,15 @@ int countPairs1(int *arr, int len, int value) {
     return k;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int k = 0;
+  for (int i = 0; i< len - 1; i++) {
+      for ( int j = len - 1; i < j; j--) {
+          if (arr[i] + arr[j] == value) {
+              k++;
+          }
+      }
+  }
+  return k;
 }
 
 int cbinsearch(int* arr, int size, int value) {
