01a692b33469d594a2cadcd57bc6bf8616d56432
SergeySokolov54 <116427633+SergeySokolov54@users.noreply.github.com>
2023-03-24 01:42:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@ int countPairs1(int *arr, int len, int value) {
     for (int i = 0; i < len - 1; i++) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
-                k += 1;
+                k++;
             }
         }
     }
